b520dfbf0261490bd4698677525eccb1e9b4274d
Brenorp <brenorpedrosa@gmail.com>
2024-06-08 13:39:59 -0300

    calculators fix

--- app/services/calculator.ts
+++ app/services/calculator.ts
@@ -1,3 +1,5 @@
+import { middleware } from "../../start/kernel.js";
+
 export interface calculatorInputType {
     num1: number;
     num2: number;
@@ -17,19 +19,78 @@ export interface calculatorResult {
 export class CalculatorService {
 
     async calculateLvl1({ num1, num2 }: calculatorInputType): Promise<calculatorResult>{
-        return { result: num1 + num2 };
+        let attempts: number;
+        switch(num1) { 
+            case 1: { 
+                attempts = 4;
+            } 
+            case 2: { 
+                attempts = 20; 
+            }
+            case 3: { 
+                attempts = 85;
+             } 
+             case 4: { 
+                attempts = 337;
+             }
+             case 5: { 
+                attempts = 1354
+             }    
+            default: {
+                attempts = 0; 
+                break;
+            }
+        }
+        return { result: num2*attempts };
     }
     
     async calculateLvl2({ materialBase, materialHd, bsb, hammerAttemptCost }: lvl2InputType): Promise<calculatorResult> {
-        return { result: materialBase + materialHd + bsb + hammerAttemptCost };
+        let attemptsTo9: number;
+        let attemptsTo10: number;
+        let bsbTo11: number;
+        let bsbTo12: number;
+        let gradingCosts: number = 16.7 + 21 + 20.53 + 77;
+
+        attemptsTo9 = materialBase*79;
+        attemptsTo10 = materialBase*284;
+        bsbTo11 = (bsb * 4 + materialHd) * 5;
+        bsbTo12 = (bsb * 7 + materialHd) * 5;
+
+
+        return { result: attemptsTo9 + attemptsTo10*3 + bsbTo11*2 + bsbTo12 + gradingCosts };
     }
     
     async calculateShadowGear({ num1, num2 }: calculatorInputType): Promise<calculatorResult>{
-        return { result: num1 + num2 };
+        let atSix: number;
+        let atSeven: number;
+
+        atSix = num1*10.23 + num2*1.23;
+        atSeven = num1*7 + num2*1.67;
+
+        return { result: Math.min(atSix, atSeven) };
     }
     
-    async calculateUpgradeEnchantment({ num1, num2 }: calculatorInputType): Promise<calculatorResult>{
-        return { result: num1 + num2 };
+    async calculateUpgradeEnchantment({ num1 }: calculatorInputType): Promise<calculatorResult>{
+
+        switch(num1) { 
+            case 2: { 
+                return { result: 6 };
+            } 
+            case 3: { 
+                return { result: 25 }; 
+            }
+            case 4: { 
+                return { result: 84 }; 
+             } 
+             case 5: { 
+                return { result: 293 };
+             }  
+            default: { 
+                break;
+            }
+
+        }
+        return { result: 0 };
     }
 }
 

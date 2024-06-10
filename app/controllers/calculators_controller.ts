2c78117ce8db7bfa16b60873554d5d4a3777aa05
Brenorp <brenorpedrosa@gmail.com>
2024-06-10 19:54:35 -0300

    fix variable names

--- app/controllers/calculators_controller.ts
+++ app/controllers/calculators_controller.ts
@@ -6,8 +6,8 @@ export default class CalculatorsController {
     @inject()
     async calculateLvl1({ request, response }: HttpContext, calculatorService: CalculatorService) {
         try {
-            const { num1, num2 } = request.body();
-            return response.header('Content-type', 'application/json').status(200).send(await calculatorService.calculateLvl1({num1, num2}))
+            const { targetStep, materialBase } = request.body();
+            return response.header('Content-type', 'application/json').status(200).send(await calculatorService.calculateLvl1({targetStep, materialBase}))
         } catch (error) {
             return response.header('Content-type', 'application/json').status(400).send({error})
         }
@@ -16,8 +16,8 @@ export default class CalculatorsController {
     @inject()
     async calculateLvl2({ request, response }: HttpContext, calculatorService: CalculatorService) {
         try {
-            const { materialBase, materialHd, bsb, hammerAttemptCost } = request.body();
-            return response.header('Content-type', 'application/json').status(200).send(await calculatorService.calculateLvl2({materialBase, materialHd, bsb, hammerAttemptCost}))
+            const { materialBase, materialHd, bsb } = request.body();
+            return response.header('Content-type', 'application/json').status(200).send(await calculatorService.calculateLvl2({materialBase, materialHd, bsb}))
         } catch (error) {
             return response.header('Content-type', 'application/json').status(400).send({error})
         }
@@ -26,8 +26,8 @@ export default class CalculatorsController {
     @inject()
     async calculateShadowGear({ request, response }: HttpContext, calculatorService: CalculatorService) {
         try {
-            const { num1, num2 } = request.body();
-            return response.header('Content-type', 'application/json').status(200).send(await calculatorService.calculateShadowGear({num1, num2}))
+            const { materialBase, shadowHammer } = request.body();
+            return response.header('Content-type', 'application/json').status(200).send(await calculatorService.calculateShadowGear({materialBase, shadowHammer}))
         } catch (error) {
             return response.header('Content-type', 'application/json').status(400).send({error})
         }

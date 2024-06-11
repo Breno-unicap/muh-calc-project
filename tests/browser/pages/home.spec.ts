59a64e0b81ee76f5a6051b6d037cc6a75ec110f6
renatosbz <rrenata12@hotmail.com>
2024-06-10 21:27:46 -0300

    more fixes

--- tests/browser/pages/home.spec.ts
+++ tests/browser/pages/home.spec.ts
@@ -31,9 +31,67 @@ test.group('Selects', () => {
   })
 
   test.group('Rejects negative', () => {
-    test('lv 1 rejects negative', async ({ visit }) => {
+    test('lv 1 rejects negative values', async ({ visit }) => {
       const page = await visit('/lvl1')
       await page.assertInputValue('input[name="materialBase"]', '-1')
+    })
+
+    test('lv 2 rejects negative values base', async({ visit }) => {
+        const page = await visit('/lvl2')
+        await page.assertInputValue('input[name="materialBase"]', '-1')
+    })
+
+    test('lv 2 rejects negative values hd', async({ visit }) => {
+        const page = await visit('/lvl2')
+        await page.assertInputValue('input[name="materialHd"]', '-1')
+    })
+
+    test('lv 2 rejects negative values bsb', async({ visit }) => {
+        const page = await visit('/lvl2')
+        await page.assertInputValue('input[name="bsb"]', '-1')
+    })
+
+    test('shadow gear rejects negative base', async({ visit }) => {
+        const page = await visit('/shadow-gear')
+        await page.assertInputValue('input[name="materialBase"]', '-1')
+    })
+
+    test('shadow gear rejects negative hammer', async({ visit }) => {
+        const page = await visit('/shadow-gear')
+        
+        await page.assertInputValue('input[name="shadowHammer"]', '-1')
+    })
+  })
+
+  test.group('Accepts float', () => {
+    test('lv 1 float', async ({ visit }) => {
+        const page = await visit('/lvl1')
+        await page.assertInputValue('input[name="materialBase"]', '1')
+    })
+
+    test('lv 2 float bsb', async({ visit }) => {
+        const page = await visit('/lvl2')
+        await page.assertInputValue('input[name="bsb"]', '2')
+    })
+
+    test('lv 2 float hd', async({ visit }) => {
+        const page = await visit('/lvl2')
+        await page.assertInputValue('input[name="materialHd"]', '3')
+    })
+
+    test('lv 2 float base', async({ visit }) => {
+        const page = await visit('/lvl2')
+        await page.assertInputValue('input[name="materialBase"]', '8.1')
+    })
+
+    test('shadow gear float base', async({ visit }) => {
+        const page = await visit('/shadow-gear')
+        await page.assertInputValue('input[name="materialBase"]', '8.1')
+    })
+
+    test('shadow gear float hammer', async({ visit }) => {
+        const page = await visit('/shadow-gear')
+        await page.assertInputValue('input[name="shadowHammer"]', '8.1')
     })
   })
   
